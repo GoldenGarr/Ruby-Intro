c28ef4a37b9c1806c51e112fd7fc45b57f41d6a8
GoldenGarr <maxim20031128@gmail.com>
2022-07-17 18:24:45 +0300

    Initial 4th homework commit

---
+++ homework/hw4/IndifferentHash.rb
@@ -0,0 +1,20 @@
+
+class HashWithIndifferentAccess_ < Hash
+
+  def [](key)
+
+      self[key.is_a? Symbol ? key.to_s : key]
+
+  end
+
+end
+
+class Hash
+  def with_indifferent_access
+    HashWithIndifferentAccess_.new(self)
+  end
+end
+
+h = { :a => 1, 'b' => 2 }.with_indifferent_access
+p h
+
