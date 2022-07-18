37aa69aa64dfebbe8c9fa3bc95272354602e40b7
GoldenGarr <maxim20031128@gmail.com>
2022-07-18 10:00:09 +0300

    Finish homework 4

--- homework/hw1/hw1.rb
+++
@@ -1,6 +0,0 @@
-def arr_sum(arr)
-  arr.length == 0 ? 0 : arr.shift + arr_sum(arr)
-end
-
-
-puts arr_sum([1, 2, 3, 4, 5])
--- homework/hw4/IndifferentHash.rb
+++ homework/hw4/IndifferentHash.rb
@@ -1,20 +1,53 @@
+class HashWithIndifferentAccess < Hash
+  alias parent_hash []
 
-class HashWithIndifferentAccess_ < Hash
+  def initialize(constructor = {})
+    if constructor.respond_to?(:to_hash)
+      super()
+      update(constructor)
+    else
+      super(constructor)
+    end
+  end
 
   def [](key)
-
-      self[key.is_a? Symbol ? key.to_s : key]
-
+    if self.key?(key.to_s)
+      self.parent_hash(key.is_a?(Symbol) ? key.to_s : key)
+    elsif self.key?(key.to_sym)
+      self.parent_hash(key.is_a?(String) ? key.to_sym : key)
+    end
   end
 
 end
 
 class Hash
   def with_indifferent_access
-    HashWithIndifferentAccess_.new(self)
+    HashWithIndifferentAccess.new(self)
   end
 end
 
-h = { :a => 1, 'b' => 2 }.with_indifferent_access
+# module IndifferentHashUtil
+#   def with_indifferent_access
+#     HashWithIndifferentAccess.new(self)
+#   end
+# end
+#
+# Hash.include(IndifferentHashUtil)
+
+h = { :a => 1, "b" => 2 }
+h = h.with_indifferent_access
+# h = HashWithIndifferentAccess.new({ :a => 1, 'b' => 2 })
+
+# p h.is_a? HashWithIndifferentAccess
+
 p h
+# p Hash.new(h)
+
+# # Regular
+p h[:a]
+p h["b"]
+
+# # Indifference showcase
+p h["a"]
+p h[:b]
 
