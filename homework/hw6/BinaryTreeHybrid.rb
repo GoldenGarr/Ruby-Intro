490b026d1b7f38b0e6d52ea4273c1040feaed0bd
GoldenGarr <maxim20031128@gmail.com>
2022-07-25 11:38:01 +0300

    Add VersionManager

---
+++ homework/hw6/BinaryTreeHybrid.rb
@@ -0,0 +1,113 @@
+=begin
+Нужно реализовать класс BinaryTree, в котором можно будет хранить элементы Node.
+У класса должны быть методы для добавления элемента в дерево, удаления и поиска элемента.
+За тип данных можно принять Integer
+=end
+
+
+class Node
+  def initialize(value, left = nil, right = nil)
+    @value = value
+    @left = left
+    @right = right
+  end
+
+  def add(other_value)
+    iter = self
+    prev = nil
+
+    while iter != nil
+      prev = iter
+      iter = iter.value < other_value ? iter.right : iter.left
+    end
+
+    if prev.value < other_value
+      prev.right = Node.new(other_value)
+    else
+      prev.left = Node.new(other_value)
+    end
+  end
+
+  # def add_recursive(other_value)
+  #   if other_value < value
+  #     left.nil? ? @left = Node.new(value) : left.add_recursive(other_value)
+  #   elsif other_value > value
+  #     right.nil? ? @right = Node.new(value) : right.add_recursive(other_value)
+  #   else
+  #     nil
+  #   end
+  # end
+
+  public attr_accessor :value, :left, :right
+end
+
+class BinaryTree
+  attr_accessor :root
+
+  def initialize
+    @root = nil
+  end
+
+  def add(value)
+    iter = root
+
+    if iter == nil
+      @root = Node.new(value)
+    else
+      root.add(value)
+      # root.add_recursive(value)
+      # prev
+      # while iter != nil
+      #   prev = iter
+      #   iter = iter.value < value ? iter.right : iter.left
+      # end
+      # prev.add(value)
+    end
+  end
+
+  def find(value)
+    iter = root
+    return if iter.nil?
+
+    while iter != nil && iter.value != value
+      iter = iter.value < value ? iter.right : iter.left
+    end
+    iter != nil && iter.value == value ? iter.value : nil
+  end
+
+  def delete(value)
+
+  end
+
+  def to_s(node)
+    if node.nil?
+      return
+    end
+    p node.value, " | "
+    to_s(node.left)
+    to_s(node.right)
+  end
+
+  # c Node
+  # def to_s
+  #   @node.to_s
+  # end
+
+end
+
+tree = BinaryTree.new
+
+tree.add(10)
+tree.add(20)
+tree.add(30)
+tree.add(40)
+tree.add(34)
+tree.add(5)
+
+p tree.find(20)
+p tree.find(40)
+p tree.find(35) #...
+p tree.find(34)
+p tree.find(10)
+
+p tree
---
+++ homework/hw6/VersionManager.rb
@@ -0,0 +1,86 @@
+# Нужно реализовать класс VersionManager. Конструктор опционально должен принимать строку - версию.
+# Допустимые варианты строки: “{major}” (“1”), “{major}.{minor}” (“1.1”), “{major}.{minor}.{patch}” (“1.1.1”).
+# В варианте типа "1.1.1.1.1" оставить только первые три значения версии.
+# Если часть версии - не целое число (напр. "а.4.е"), то генерировать ошибку.
+# Если версия не указана, то значение по умолчанию - “0.0.1”.
+
+# Тестовые варианты
+
+# vm = VersionManager.new
+
+# vm.major! # увеличивает мажорную версию, устанавливая минорную и патч в 0 (напр. “2.0.0”)
+# vm.minor! # увеличивает минорную версию, устанавливая патч в 0 (напр. “2.1.0”)
+# vm.patch! # увеличивает патч версию (напр. “2.1.1”)
+
+# vm.rollback! # возвращается к прошлой версии, если это возможно, иначе выдать ошибку
+# vm.release # возвращает текущую версию в формате “{major}.{minor}.{patch}”
+
+class VersionManager
+  public attr_accessor :major, :minor, :patch
+
+  def initialize(version = "0.0.1")
+    parsed = version.split(".")
+
+    if is_integer(parsed)
+      @major = parsed.length > 0 ? parsed[0].to_i: 0
+      @minor = parsed.length > 1 ? parsed[1].to_i : 0
+      @patch = parsed.length > 2 ? parsed[2].to_i : 0
+    end
+
+    is_valid
+  end
+
+  def is_valid
+    if @major < 0 ||
+        @minor < 0 || @patch < 0
+      raise 'Invalid version'
+    end
+  end
+
+  def release
+    [major, minor, patch].join(".")
+  end
+
+  def is_integer(parsed)
+    result = true
+    parsed.each do |version|
+      result &&= (version.to_i.to_s == version.to_s) || version == ""
+    end
+    result
+  end
+
+  def major!
+    @major += 1
+    @minor = @patch = 0
+  end
+
+  def minor!
+    @minor += 1
+    @patch = 0
+  end
+
+  def patch!
+    @patch += 1
+  end
+
+  def rollback!
+    raise 'Can not rollback to previous version'
+  end
+end
+
+# manager = VersionManager.new("1.1.1")
+# manager = VersionManager.new("a.1.1")
+# manager = VersionManager.new("1.-2.1")
+# manager = VersionManager.new
+manager = VersionManager.new(".1.1")
+p manager.release # => 0.1.1
+
+
+manager.patch!
+p manager.release # => 0.1.2
+
+manager.minor!
+p manager.release # => 0.2.0
+
+manager.major!
+p manager.release # => 1.0.0
