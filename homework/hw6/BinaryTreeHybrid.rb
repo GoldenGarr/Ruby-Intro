3c06c260fae3a4a9a081f86a7034061bade37709
GoldenGarr <maxim20031128@gmail.com>
2022-07-25 13:06:51 +0300

    FInish Binary Tree

--- homework/hw6/BinaryTreeHybrid.rb
+++ homework/hw6/BinaryTreeHybrid.rb
@@ -16,88 +16,72 @@ class BinaryTree
   def add(value)
     iter = root
 
-    if iter == nil
+    if iter.nil?
       @root = Node.new(value)
     else
-      root.add(value)
+      @root.add(value)
       # root.add_recursive(value)
     end
   end
 
-  def find(value)
-    iter = root
-    return if iter.nil?
-
-    while iter != nil && iter.value != value
-      iter = iter.value < value ? iter.right : iter.left
+  def find(node = root, value)
+    return nil if node.nil?
+    if value < node.value
+      find(node.left, value)
+    elsif value > node.value
+      find(node.right, value)
+    else
+      value
     end
-    iter != nil && iter.value == value ? iter.value : nil
   end
 
-  def delete(value)
-    sub_delete(root, value)
-  end
-
-  def sub_delete(node, value)
+  def delete(node = root, value)
     if node == nil
       return node
     end
     if value < node.value
-      node.left = sub_delete(node.left, value)
+      node.left = delete(node.left, value)
     elsif value > node.value
-      node.right = sub_delete(node.right, value)
+      node.right = delete(node.right, value)
     elsif node.left != nil && node.right != nil
       node.value = get_minimum(node.right).value
-      node.right = sub_delete(node.right, value)
+      node.right = delete(node.right, node.value)
     else
       if node.left != nil
-        node.value = node.left.value
-        node.right = node.left.right
-        node.left = node.left.left
-        # node = node.left
+        node = node.left
       elsif node.right != nil
-        node.value = node.right.value
-        node.left = node.right.left
-        node.right = node.right.right
-        # node = node.right
+        node = node.right
       else
-        @node = nil
+        node = nil
       end
     end
+    node
   end
 
-  def get_minimum(node)
-    if node.left == nil
-      return node
-    end
+  def get_minimum(node = root)
+    return if node.nil?
+    return node if node.left.nil?
+
     get_minimum(node.left)
-    # if node == nil
-    #   return nil
-    # elsif node.left == nil
-    #   node
-    # end
-    # get_minimum(node.left)
   end
-
 
   def to_s
     centered_print(root)
   end
 
   # An ordered debug print
-  def centered_print(node)
+  def centered_print(node = root)
     if node == nil
       p "empty"
     end
-    if node.left != nil
+    unless node.left.nil?
       centered_print(node.left)
     end
     puts node.value.to_s
-    if node.right != nil
+    unless node.right.nil?
       centered_print(node.right)
     end
   end
-
 end
 
 tree = BinaryTree.new
@@ -109,14 +93,23 @@ tree.add(40)
 tree.add(34)
 tree.add(5)
 
+# p tree.get_minimum
+
 # p tree.find(20)
 # p tree.find(40)
 # p tree.find(35) #...
 # p tree.find(34)
 # p tree.find(10)
+# p tree.find(5)
+
+tree.centered_print(tree.root)
+puts "_____"
 
 tree.delete(10)
 tree.delete(20)
+tree.delete(30)
+tree.delete(5)
+
 
 # tree.sub_delete(tree.root, 10)
 
--- homework/hw6/Node.rb
+++ homework/hw6/Node.rb
@@ -6,30 +6,14 @@ class Node
   end
 
   def add(other_value)
-    iter = self
-    prev = nil
-
-    while iter != nil
-      prev = iter
-      iter = iter.value < other_value ? iter.right : iter.left
-    end
-
-    if prev.value < other_value
-      prev.right = Node.new(other_value)
+    if other_value < value
+      left.nil? ? @left = Node.new(other_value) : left.add(other_value)
+    elsif other_value > value
+      right.nil? ? @right = Node.new(other_value) : right.add(other_value)
     else
-      prev.left = Node.new(other_value)
+      nil
     end
   end
-
-  # def add_recursive(other_value)
-  #   if other_value < value
-  #     left.nil? ? @left = Node.new(value) : left.add_recursive(other_value)
-  #   elsif other_value > value
-  #     right.nil? ? @right = Node.new(value) : right.add_recursive(other_value)
-  #   else
-  #     nil
-  #   end
-  # end
 
   public attr_accessor :value, :left, :right
 end
